af4d3bffa05609323b32b976410f7d1de728b7f6
Eugene Wu <ewu@cs.columbia.edu>
2022-09-29 13:13:29 -0400

    lazy commit msg

---
+++ src/qs.ts
@@ -0,0 +1,28 @@
+
+export const queries = [
+  {
+    name: "select all",
+    q: "select * from sailors"
+  },
+  {
+    name: "groupby having",
+    q: `SELECT s.sid, sum(s.age)
+FROM sailors as s, reserves as r
+WHERE s.sid = r.sid
+GROUP BY s.sid
+HAVING count(1) > 1`
+  },
+  {
+    name: "subquery join",
+    q: `SELECT c, sum(x)
+FROM (SELECT a*b as x, c, d  FROM data) AS d2 
+GROUP BY c`
+  },
+  {
+    name: "subquery join",
+    q: `SELECT a, sum(b+2) * 2 as c 
+FROM data, (SELECT a*b as x, c, d  FROM data) AS d2 
+WHERE data.b = d2.x
+GROUP BY a`
+  }
+]
